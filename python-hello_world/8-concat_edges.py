900dab1c77d60aec366ff16fbd86ed9d94ca5d98
umudovafatima <umudovafatimee@gmail.com>
2024-09-03 20:29:36 +0400

    asdfghjkl

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1=str[39:67] + 'with' + str[:6]
-print(str1)
+str = str[39:67] + 'with' + str[:6]
+print(str)
