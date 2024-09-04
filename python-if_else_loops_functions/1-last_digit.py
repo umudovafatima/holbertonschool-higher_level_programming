76ee8d73833e996e45b965d4e8fe08b869a917d8
umudovafatima <umudovafatimee@gmail.com>
2024-09-04 17:52:51 +0400

    salam

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 salam = number % 10
-if salam>5:
+if salam > 5:
     print(f'Last digit of {number} is {salam} and is greater than 5')
-elif salam==0:
+elif salam == 0:
     print(f'Last digit of {number} is {salam} and is 0')
 else:
     print(f'Last digit of {number} is {salam} and is less than 6 and not 0')
