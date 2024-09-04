46210ba654aee42e9a331c69f552cec3db1e8512
umudovafatima <umudovafatimee@gmail.com>
2024-09-04 20:51:44 +0400

    asdf

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3   
 for letters in range(ord('a'), ord('z')+1):
     if letters >= ord('a') and letters <= ord('z'):
-        print(f'{chr(letters)}', end="")
+        print("{}".format(chr(letters)), end="")
