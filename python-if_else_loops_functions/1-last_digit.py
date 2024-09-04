16b1ac3e7b8dc31072da77e2bcc8415bf708bf40
umudovafatima <umudovafatimee@gmail.com>
2024-09-04 19:44:18 +0400

    gfdsa

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-salam = number % 10
+salam = abs(number) % 10
+
 if salam > 5:
     print(f'Last digit of {number} is {salam} and is greater than 5')
 elif salam == 0:
