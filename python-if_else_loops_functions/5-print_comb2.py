7299f8c3413562131fd19259bba5c074f598e45c
umudovafatima <umudovafatimee@gmail.com>
2024-09-05 19:09:59 +0400

    asdfghj

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i >= 0 and i < 10:
-        print(str(i).zfill(2), end=',')
-    else:
-        print(i, end=',')
+    print("{:02}".format(i), end=', ' if i < 99 else '\n')
