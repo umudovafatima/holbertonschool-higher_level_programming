5370361ddd03187e22db67184c172a4678b9b35b
umudovafatima <umudovafatimee@gmail.com>
2024-09-05 19:17:48 +0400

    asdfg

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3   
+#!/usr/bin/python3
 for letters in range(ord('a'), ord('z')+1):
     if letters >= ord('a') and letters <= ord('z'):
-        print("{}".format(chr(letters)), end = "")
+        print("{}".format(chr(letters)), end="")
---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+l=[0,1,2,3,4,5,6,7,8,9]
+for l in range(0,10):
+    l=
