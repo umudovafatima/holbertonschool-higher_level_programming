f2b637652a00d07389725455d270c5f7be6c007d
umudovafatima <umudovafatimee@gmail.com>
2024-09-05 16:45:16 +0400

    asdfgh

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3   
+#!/usr/bin/python3
 for letters in range(ord('a'), ord('z')+1):
     if chr(letters) == 'q' or chr(letters) == 'e':
         pass
     else:
-        print("{}".format(chr(letters)), end = "")
+        print("{}".format(chr(letters)), end="")
