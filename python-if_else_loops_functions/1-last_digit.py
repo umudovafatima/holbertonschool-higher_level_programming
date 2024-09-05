ba22d89b4a7188b2af4a2b4c5b727c4cb29db003
umudovafatima <umudovafatimee@gmail.com>
2024-09-05 20:11:30 +0400

    asdfg

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 salam = abs(number) % 10
-
+if number < 0:
+    salam = -salam
 if salam > 5:
     print(f'Last digit of {number} is {salam} and is greater than 5')
 elif salam == 0:
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-l=[0,1,2,3,4,5,6,7,8,9]
-for l in range(0,10):
-    l=
+l = [0,1,2,3,4,5,6,7,8,9]
+for i in range l:
+    print("{:02d}".format(i), end=', ' if i < 99 else '\n')
