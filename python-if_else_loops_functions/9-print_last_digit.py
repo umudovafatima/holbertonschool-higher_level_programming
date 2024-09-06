39d4c274761f450988a468211c76a59211fc1808
umudovafatima <umudovafatimee@gmail.com>
2024-09-06 14:48:36 +0400

    asdfgb

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number< 0:
+    if number < 0:
         number = -number
     salam = abs(number) % 10
-    print(f'{salam}',end='')
+    print(f'{salam}', end='')
     return salam
