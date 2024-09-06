8a7cf1c87061c960a2883281bcb6c1273a6f4f0e
umudovafatima <umudovafatimee@gmail.com>
2024-09-06 13:34:36 +0400

    assdfg

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-l = [0,1,2,3,4,5,6,7,8,9]
-for i in range l:
-    print("{:02d}".format(i), end=', ' if i < 99 else '\n')
+lis = [0,1,2,3,4,5,6,7,8,9]
+list = []
+for sal in range (10):
+    
+    print(list[i])
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for letters in range(ord('a'), ord('z')+1):
+    if letters >= ord('a') and letters <= ord('z'):
+        print("a is {}".format(chr(letters)))
