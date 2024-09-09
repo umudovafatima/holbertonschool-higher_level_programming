118595411c4287c154b489931f8305759ea568b6
umudovafatima <umudovafatimee@gmail.com>
2024-09-10 00:31:09 +0400

    task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,5 +2,12 @@
 from sys import argv
 
 if __name__ == "__main__":
+    number = len(argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
