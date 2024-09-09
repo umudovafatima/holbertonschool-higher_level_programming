fb1b9b1c981e94783d6d177d2f2c5cbefb4531ff
umudovafatima <umudovafatimee@gmail.com>
2024-09-09 20:41:26 +0400

    asd

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add,sub,mul,div
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 x = add(a, b)
-y = sub(a,b)
-k = mul(a,b)
-j = div(a,b)
+y = sub(a, b)
+k = mul(a, b)
+j = div(a, b)
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, x))
     print("{} - {} = {}".format(a, b, y))
