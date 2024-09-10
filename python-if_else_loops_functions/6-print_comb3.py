82823fc0a0bd1abb46ae7784b18e0fc1d3b99c87
umudovafatima <umudovafatimee@gmail.com>
2024-09-10 19:28:13 +0400

    asdf

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 for i in range(1, 100):
-    if i % 10 > i // 10: 
-        if i < 89: 
+    if i % 10 > i // 10:
+        if i < 89:
             print("{:02d}, ".format(i), end="")
         else:
             print("{:02d}".format(i))
